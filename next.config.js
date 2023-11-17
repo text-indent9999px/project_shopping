25265b1956fa5fb19f307689d50499f790bf4115
namyoung <namyoung1013@gmail.com>
2023-11-17 16:32:33 +0900

    next.config.js 파일 수정

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     /* config options here */
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-                port: '',
-                pathname: '',
-            },
-        ],
-    },
 }
 
 module.exports = nextConfig
